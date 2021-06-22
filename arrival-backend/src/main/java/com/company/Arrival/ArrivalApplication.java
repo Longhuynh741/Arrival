c705c4039ce7982ed1600d99392283748e96e9d1
Long Huynh <Longhuynh741@yahoo.com>
2021-06-22 10:59:58 -0400

    first commit. created react app folder for front-end and a Spring Project for back-end

---
+++ arrival-backend/src/main/java/com/company/Arrival/ArrivalApplication.java
@@ -0,0 +1,13 @@
+package com.company.Arrival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrivalApplication.class, args);
+	}
+
+}
---
+++ arrival-backend/src/test/java/com/company/Arrival/ArrivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Arrival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
