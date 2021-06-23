b0099497f16265c2a1e05be249a0202b3c6a2b0c
naritchaya louis <naritchayalouis@gmail.com>
2021-06-23 12:33:31 -0500

    added CheckIn page

--- arrival-frontend/src/App.js
+++ arrival-frontend/src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Jumbotron } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import CheckIn from "./pages/CheckIn";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
+    
     <div className="App">
-      <Jumbotron> Hello World</Jumbotron>
+    
+    <CheckIn></CheckIn>
+      
     </div>
+    
   );
 }
 
