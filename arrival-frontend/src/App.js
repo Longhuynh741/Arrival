903e8fd3111b7ce921b0cd93449895efaa021831
Long Huynh <Longhuynh741@yahoo.com>
2021-06-23 15:49:09 -0400

    added the routers and switches for our App.js

--- arrival-frontend/src/App.js
+++ arrival-frontend/src/App.js
@@ -3,14 +3,30 @@ import Homepage from './pages/Homepage';
 import EventResults from './pages/Results';
 import Event from './pages/Event';
 import CheckIn from './pages/CheckIn';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Homepage></Homepage>
-      <Event></Event>
-      <CheckIn></CheckIn>
-      <EventResults> </EventResults>
-    </div>
+    <Router> 
+      <div className="App">
+        <Route exact path= {["/", "/Arrival"]}>
+      <Homepage/>
+        </Route>
+
+        <Route exact path= {["/Event"]}>
+          <Event />
+          </Route>
+
+          <Route exact path= {["/CheckIn"]}>
+            <CheckIn />
+            </Route>
+
+      <Route exact path= {["/Results"]}>
+        <EventResults />
+        </Route>
+
+      </div>  
+      </Router>
   );
 }
 
