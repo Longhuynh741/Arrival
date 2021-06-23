d71d4b6d6543ccbc5d0d6fa0eb2f9717b964863b
Omar Cossio <omar119mce@gmail.com>
2021-06-23 12:31:52 -0400

    added content to Event.js page

---
+++ arrival-frontend/src/pages/Event.js
@@ -0,0 +1,73 @@
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Jumbotron } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+const Event = (props) => {
+  return (
+    <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <h1>Coming Up Events</h1>
+        </Row>
+        <Jumbotron>
+          <Row className="justify-content-center">
+           
+            <Col md="auto">
+              <Table responsive="sm, md, lg">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th colSpan="2">Event</th>
+                    <th colSpan="2">Main Host</th>
+                    <th colSpan="1">Location</th>
+                    <th colSpan="2">Details</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td colSpan="2">Mark</td>
+                    <td colSpan="2">Otto</td>
+                    <td colSpan="2">@mdo</td>
+                    <td colSpan="2">More Details</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td colSpan="2">Jacob</td>
+                    <td colSpan="2">Thornton</td>
+                    <td colSpan="2">@fat</td>
+                    <td colSpan="2">More Details</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry</td>
+                    <td colSpan="2">The Bird</td>
+                    <td colSpan="2">@twitter</td>
+                    <td colSpan="2">More Details</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          
+          </Row>
+        </Jumbotron>
+
+        <Row className="justify-content-md-center">
+          <Col>
+            <h4>Add Event</h4>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col>
+            <Button variant="add">Info</Button>{" "}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Event;
