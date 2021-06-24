11e32a51714bddfa0bf8ac01a3a588a3e8bb4df3
Long Huynh <Longhuynh741@yahoo.com>
2021-06-24 13:14:17 -0400

    first commit

---
+++ arrival-backend/arrival-cloud-config-service/src/main/java/com/company/arrivalcloudconfigservice/ArrivalCloudConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.arrivalcloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrivalCloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrivalCloudConfigServiceApplication.class, args);
+	}
+
+}
