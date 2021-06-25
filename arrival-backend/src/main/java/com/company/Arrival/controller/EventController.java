c316c26b7e8be2bcade05cfa1e5dc16e8e101a19
Long Huynh <Longhuynh741@yahoo.com>
2021-06-25 15:16:20 -0400

    fixed post route in controller. commented out config files for now

--- arrival-backend/src/main/java/com/company/Arrival/controller/EventController.java
+++ arrival-backend/src/main/java/com/company/Arrival/controller/EventController.java
@@ -3,33 +3,32 @@ import com.company.Arrival.dao.EventRepository;
 import com.company.Arrival.dto.Event;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/events")
-@RefreshScope
+//@RefreshScope
 public class EventController {
-
     @Autowired
     private EventRepository eventRepo;
-
 
     @PostMapping("/events")
     @ResponseStatus(HttpStatus.CREATED)
     public Event createEvent(@RequestBody Event event) {
         System.out.println("Creating Event: " + event);
-        return event;
+        return eventRepo.save(event);
     }
 
-//    @GetMapping
-//    public List<Event> getAllEvents() {
-//        return eventRepo.findAll();
-//    }
+    @GetMapping(value = "/events")
+    public List<Event> getAllEvents() {
+
+        return eventRepo.findAll();
+    }
 
     @GetMapping(value = "/{id}")
     public Event getEventById(@PathVariable int eventId) {
