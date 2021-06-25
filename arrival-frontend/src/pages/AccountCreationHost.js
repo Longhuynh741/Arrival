01861895a44bd23f2e2222f07b8f916086c5584a
naritchaya louis <naritchayalouis@gmail.com>
2021-06-24 20:52:17 -0500

    enabled validate form deleted form field

--- arrival-frontend/src/pages/AccountCreationHost.js
+++ arrival-frontend/src/pages/AccountCreationHost.js
@@ -1,64 +1,54 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useHistory } from "react-router";
-import useFormFields from '@usereact/use-form-fields';
 const AccountCreationHost = () => {
-  const [fields, handleFieldChange] = useState({
-      email: "",
-      password: "",
-      confirmPassword: "",
-  });
-  const history = useHistory();
-  const [newUser, setNewUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-//   function validateForm() {
-//     return (
-//         fields.email.length > 0 && 
-//         fields.password.length > 0 &&
-//         fields.password === fields.confirmPassword
-//     );
-//   }
- async function handleSubmit(event) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const validateForm = () => {
+    return (
+      email.length > 0 && password.length > 0 && password === confirmPassword
+    );
+  };
+  function handleSubmit(event) {
     event.preventDefault();
-    setIsLoading(true);
-    setNewUser("test");
-    setIsLoading(false);
   }
   return (
-    <Form onSubmit={handleSubmit}>
+    <div className="createAccount">
+      <h1>Create Your Host Account!</h1>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="email" size="lg">
-            <Form.Label>Eamil</Form.Label>
-            <Form.Control
+          <Form.Label>Email</Form.Label>
+          <Form.Control
             autoFocus
             type="email"
-            value={fields.email}
-            onChange={handleFieldChange}
-            />
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="password" size="lg">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
+          <Form.Label>Password</Form.Label>
+          <Form.Control
             autoFocus
             type="password"
-            value={fields.password}
-            onChange={handleFieldChange}
-            />
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="confirmPassword" size="lg">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
             autoFocus
             type="password"
-            value={fields.confirmPassword}
-            onChange={handleFieldChange}
-            />
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </Form.Group>
-        <Button variant="info" type="submit">
-        {/* <Button variant="info" type="submit" disabled={!validateForm()}> */}
+        <Button variant="info" type="submit" disabled={!validateForm()}>
           Sign Up
         </Button>
       </Form>
+    </div>
   );
-}
+};
 export default AccountCreationHost;
