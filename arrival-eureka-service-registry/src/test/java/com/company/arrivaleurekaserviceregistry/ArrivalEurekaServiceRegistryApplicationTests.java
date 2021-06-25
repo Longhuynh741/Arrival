49d6b75a7c555fb55917fa6f05421c911130c6eb
Long Huynh <Longhuynh741@yahoo.com>
2021-06-25 09:22:46 -0400

    created arrival eureka service registry

---
+++ arrival-eureka-service-registry/src/test/java/com/company/arrivaleurekaserviceregistry/ArrivalEurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.arrivaleurekaserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrivalEurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
