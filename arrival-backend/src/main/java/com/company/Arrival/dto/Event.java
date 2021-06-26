59681b1ebe5e4551dcda22ff09cfd561367d24ff
naritchaya louis <naritchayalouis@gmail.com>
2021-06-26 14:49:04 -0500

    added route search by company name

--- arrival-backend/src/main/java/com/company/Arrival/dto/Event.java
+++ arrival-backend/src/main/java/com/company/Arrival/dto/Event.java
@@ -1,21 +1,19 @@
 package com.company.Arrival.dto;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name="event")
 public class Event {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer eventId;
-    @NotEmpty(message = "You must provide the name of your company")
     private String companyName;
     @NotEmpty(message = "You must supply a name for the event")
     private String eventName;
@@ -24,93 +22,17 @@ public class Event {
     @NotEmpty(message = "You must supply a location")
     private String location;
     private String moreDetails;
-    private List<String> questions;
-
+    private ArrayList<String> questions;
     public Event() {}
-
-
-
-
-    public Event(Integer id, String companyName, String eventName, String mainHost, String location, String moreDetails, List<String> questions) {
+    public Event(Integer eventId, String companyName, String eventName, String mainHost, String location, String moreDetails, ArrayList<String> questions) {
         this.eventId = eventId;
         this.companyName = companyName;
         this.eventName = eventName;
         this.mainHost = mainHost;
         this.location = location;
         this.moreDetails = moreDetails;
-        this.questions = Collections.unmodifiableList(new ArrayList<>(questions));
-    }
-
-    public Integer getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(Integer eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getMainHost() {
-        return mainHost;
-    }
-
-    public void setMainHost(String mainHost) {
-        this.mainHost = mainHost;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getMoreDetails() {
-        return moreDetails;
-    }
-
-    public void setMoreDetails(String moreDetails) {
-        this.moreDetails = moreDetails;
-    }
-
-    public List<String> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(List<String> questions) {
         this.questions = questions;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return Objects.equals(eventId, event.eventId) && Objects.equals(companyName, event.companyName) && Objects.equals(eventName, event.eventName) && Objects.equals(mainHost, event.mainHost) && Objects.equals(location, event.location) && Objects.equals(moreDetails, event.moreDetails) && Objects.equals(questions, event.questions);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(eventId, companyName, eventName, mainHost, location, moreDetails, questions);
-    }
-
     @Override
     public String toString() {
         return "Event{" +
@@ -123,5 +45,57 @@ public class Event {
                 ", questions=" + questions +
                 '}';
     }
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return Objects.equals(getEventId(), event.getEventId()) && Objects.equals(getCompanyName(), event.getCompanyName()) && Objects.equals(getEventName(), event.getEventName()) && Objects.equals(getMainHost(), event.getMainHost()) && Objects.equals(getLocation(), event.getLocation()) && Objects.equals(getMoreDetails(), event.getMoreDetails()) && Objects.equals(getQuestions(), event.getQuestions());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEventId(), getCompanyName(), getEventName(), getMainHost(), getLocation(), getMoreDetails(), getQuestions());
+    }
+    public List<String> getQuestions() {
+        return questions;
+    }
+    public void setQuestions(ArrayList<String> questions) {
+        this.questions = questions;
+    }
+    public Integer getEventId() {
+        return eventId;
+    }
+    public void setEventId(Integer eventId) {
+        this.eventId = eventId;
+    }
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    public String getEventName() {
+        return eventName;
+    }
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+    public String getMainHost() {
+        return mainHost;
+    }
+    public void setMainHost(String mainHost) {
+        this.mainHost = mainHost;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getMoreDetails() {
+        return moreDetails;
+    }
+    public void setMoreDetails(String moreDetails) {
+        this.moreDetails = moreDetails;
+    }
 }
